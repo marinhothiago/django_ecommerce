5c5e63ddba709200c6bc971e573185c15a6b50e5
marinhothiago <thiago.marinho@email.com>
2020-09-11 01:42:07 -0300

    add url e template product

--- e_commerce/urls.py
+++ e_commerce/urls.py
@@ -4,6 +4,7 @@ from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import path
 
+from products.views import ProductListView, product_list_view
 from .views import home_page, about_page, contact_page, login_page, register_page
 
 urlpatterns = [
@@ -12,7 +13,9 @@ urlpatterns = [
 	path('contact/', contact_page),
         path('login/', login_page),
         path('register/', register_page),
-	path('admin/', admin.site.urls),
+	path('products/', ProductListView.as_view()),
+        path('products-fbv/', product_list_view),  
+        path('admin/', admin.site.urls),
 ]
 if settings.DEBUG:
     urlpatterns = urlpatterns + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
