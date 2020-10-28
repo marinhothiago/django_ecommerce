2493aa7b782a55d6c8b4d142a15219f571b21f16
marinhothiago <thiago.marinho@email.com>
2020-10-28 17:14:26 -0300

    display user query

--- search/urls.py
+++ search/urls.py
@@ -1,10 +1,10 @@
+from .views import (
+    SearchProductView,
+)
 from django.urls import path
 
 app_name = "search"
 
-from .views import (
-                        SearchProductView, 
-                    )
 urlpatterns = [
-    path('', SearchProductView.as_view(), name='list'),
+    path('', SearchProductView.as_view(), name='query'),
 ]
