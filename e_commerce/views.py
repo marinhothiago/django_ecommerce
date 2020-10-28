38b63982fd55b1b871741b3352d699393d8af7a1
marinhothiago <thiago.marinho@email.com>
2020-10-28 04:44:54 -0300

    Atualizando codigo entrega parcial

--- e_commerce/views.py
+++ e_commerce/views.py
@@ -6,8 +6,8 @@ from .forms import ContactForm, LoginForm, RegisterForm
 
 def home_page(request):
     context = {
-                    "title": "Home Page",
-                    "content": "Bem vindo a Home Page",
+                    "title": "Página principal",
+                    "content": "Bem vindo",
               }
     if request.user.is_authenticated:
         context["premium_content"] = "Você é um usuário Premium"
@@ -15,16 +15,16 @@ def home_page(request):
     
 def about_page(request):
     context = {
-                    "title": "About Page",
-                    "content": "Bem vindo a About Page"
+                    "title": "Sobre nós",
+                    "content": "Bem vindo a Página Sobre"
               }
     return render(request, "about/view.html", context)
 
 def contact_page(request):
     contact_form = ContactForm(request.POST or None)
     context = {
-                    "title": "Contact Page",
-                    "content": "Bem vindo a Contact Page",
+                    "title": "Contato",
+                    "content": "Bem vindo a página de contato",
                     "form": contact_form	
               }
     if contact_form.is_valid():
